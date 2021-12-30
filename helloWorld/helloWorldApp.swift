5416c7490d58e74844b2ed6b1bffa5d079387910
Samuel Cardoso <scksamuel.sc@gmail.com>
2021-12-30 19:24:20 -0300

    Hello world in SwiftUI with modifiers

---
+++ helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by Samuel Cardoso on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
